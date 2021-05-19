c536802ee8b5cd878575884fe6daaa0e1da4ace6
emretsdmr <emretsdmr95@gmail.com>
2021-05-19 05:49:12 +0300

    ckeditor

--- product/models.py
+++ product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.utils.safestring import mark_safe
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 
 class Category(models.Model):
@@ -35,7 +37,7 @@ class Product(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     amount = models.IntegerField()
-    detail = models.TextField()
+    detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
     created_at = models.DateTimeField(auto_now_add=True)
