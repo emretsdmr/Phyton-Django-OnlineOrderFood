bb28f34ef7b7ebc414467fe1b87ee394b96bee19
emretsdmr <emretsdmr95@gmail.com>
2021-05-20 22:21:38 +0300

    contact form

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu
 
 
 def index(request):
@@ -21,6 +22,22 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+    if request.method=='POST':
+        form=ContactFormu(request.POST)
+        #return HttpResponse(form.data['name'])
+        if form.is_valid():
+            form = ContactFormu(request.POST)
+            form.name = form.data['name']
+            form.subject = form.data['subject']
+            form.email = form.data['email']
+            form.message = form.data['message']
+            form.ip = request.META.get('REMOTE_ADDR')
+            form.save()
+            messages.success(request,'Mesajınız başarı ile gönderilmiştir.')
+            return HttpResponseRedirect('/iletisim')
+
+
     setting = Setting.objects.get(pk=1)
-    context={'setting':setting}
+    form= ContactFormu()
+    context={'setting':setting,'form':form}
     return render(request, 'iletisim.html', context)
