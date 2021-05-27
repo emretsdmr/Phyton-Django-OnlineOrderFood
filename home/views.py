868ea13941b64b0920c8f6ad72b86ef052f4f732
emretsdmr <emretsdmr95@gmail.com>
2021-05-27 06:01:48 +0300

    sidebar categories

--- home/views.py
+++ home/views.py
@@ -4,13 +4,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 from home.models import Setting, ContactFormu
-from product.models import Product
+from product.models import Product, Category
 
 
 def index(request):
     setting = Setting.objects.get(pk=1)
     sliderdata = Product.objects.all()[:4]
-    context={'setting':setting,'page': 'home','sliderdata':sliderdata}
+    category=Category.objects.all()
+
+    context={
+        'setting':setting,
+        'category':category,
+        'page': 'home',
+        'sliderdata':sliderdata
+    }
     return render(request, 'index.html', context)
 
 def hakkimizda(request):
--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 # Register your models here.
 from product.models import Category, Product, Images
 
@@ -7,7 +8,7 @@ class ProductImageInline(admin.TabularInline):
     model=Images
     extra=5
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(MPTTModelAdmin):
     list_display=['title', 'status']
     list_filter=['status']
 
@@ -18,11 +19,43 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter=['status','category']
     inlines=[ProductImageInline]
 
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Category.objects.add_related_count(
+                qs,
+                Product,
+                'category',
+                'products_cumulative_count',
+                cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                 Product,
+                 'category',
+                 'products_count',
+                 cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+    related_products_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+    related_products_cumulative_count.short_description = 'Related products (in tree)'
 
 class ImagesAdmin(admin.ModelAdmin):
     list_display=['title','product','image_tag']
     readonly_fields = ('image_tag',)
 
-admin.site.register(Category,CategoryAdmin)
+admin.site.register(Category,CategoryAdmin2)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Images,ImagesAdmin)
