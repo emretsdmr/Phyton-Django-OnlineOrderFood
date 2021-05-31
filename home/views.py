5a2a72b06c8058733f553cdfef611c8c4fb72fa9
emretsdmr <emretsdmr95@gmail.com>
2021-05-31 15:17:27 +0300

    homepage products

--- home/views.py
+++ home/views.py
@@ -11,12 +11,19 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     sliderdata = Product.objects.all()[:4]
     category=Category.objects.all()
+    dayproducts=Product.objects.all()[:4]
+    lastproducts=Product.objects.all().order_by('-id')[:4]
+    randomproducts=Product.objects.all().order_by('?')[:4]
+
 
     context={
         'setting':setting,
         'category':category,
         'page': 'home',
-        'sliderdata':sliderdata
+        'sliderdata':sliderdata,
+        'dayproducts':dayproducts,
+        'lastproducts':lastproducts,
+        'randomproducts':randomproducts
     }
     return render(request, 'index.html', context)
 
