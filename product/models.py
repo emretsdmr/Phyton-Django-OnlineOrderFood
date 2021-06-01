12b0178c653aa99c2e127bae4bb83c4a768dcbf9
emretsdmr <emretsdmr95@gmail.com>
2021-06-01 19:20:28 +0300

    automatic slug

--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 from django.forms import ModelForm
+from django.urls import reverse
 
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -42,7 +43,7 @@ class Category(MPTTModel):
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True,upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False,unique=True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -59,6 +60,8 @@ class Category(MPTTModel):
             k = k.parent
         return ' ->  '.join(full_path[::-1])
 
+    def get_absolute_url(self):
+        return reverse('category_detail',kwargs={'slug':self.slug})
 
 class Product(models.Model):
     STATUS = (
@@ -75,7 +78,7 @@ class Product(models.Model):
     amount = models.IntegerField()
     detail = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
@@ -83,6 +86,9 @@ class Product(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('product_detail',kwargs={'slug':self.slug})
 
 
 class Images(models.Model):
