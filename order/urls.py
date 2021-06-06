553efb7d4a0f68148270ac2ed943aa50991afbe1
emretsdmr <emretsdmr95@gmail.com>
2021-06-06 23:49:17 +0300

    orders

--- order/urls.py
+++ order/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('addtocart/<int:id>',views.addtocart,name='addtocart'),
     path('deletefromcart/<int:id>',views.deletefromcart,name='deletefromcart'),
+    path('orderproduct/',views.orderproduct, name="orderproduct"),
 
     #path('<int:question_id>/', views.detail, name='detail'),
 
